3e4ce8213eb6ee01de6ddae786ce80b4aa4572e6
rizkypsr <rizkypsrr@gmail.com>
2024-10-01 18:18:27 +0700

    fix

--- resources/js/Pages/Laporan/Form.tsx
+++ resources/js/Pages/Laporan/Form.tsx
@@ -26,12 +26,12 @@ import Select from "react-select";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Calendar } from "@/components/ui/calendar";
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 
 type FormProps = {
     form: UseFormReturn<z.infer<typeof FormSchema>>;
